03798755e688a9207b28274f4da5eda63ec67a33
Paper Lee <paper.li@gmail.com>
2014-08-12 00:27:20 +0800

    Add saveData method in DBManager

--- butterstill/DBManager.m
+++ butterstill/DBManager.m
@@ -35,7 +35,21 @@ static sqlite3_stmt *statement = nil;
         if (sqlite3_open(dbpath, &database) == SQLITE_OK){
             char *errMsg;
             //TODO: real database set up
-            const char *sql_stmt = "CREATE TABLE IF NOT EXISTS stillsprofile (id INTEGER PRIMARY KEY, image TEXT, audio TEXT, date TEXT)";
+            // id: int
+            // uid: int
+            // author: string
+            // description: string
+            // image: string
+            // audio: string
+            // create_date: string
+            // update_date: string
+            // sync_date: string
+            // liked: int
+            // disliked: int
+            // remote: string
+            // enable: int
+            
+            const char *sql_stmt = "CREATE TABLE IF NOT EXISTS stillsprofile (id INTEGER PRIMARY KEY, uid INTEGER, author TEXT, description TEXT, image TEXT, audio TEXT, create_date TEXT, update_date TEXT, sync_date TEXT, liked INTEGER, disliked INTEGER, remote TEXT, enable INTEGER)";
             if (sqlite3_exec(database, sql_stmt, NULL, NULL, &errMsg) != SQLITE_OK){
                 isSuccess = NO;
                 NSLog(@"Fail to create table");
@@ -49,6 +63,36 @@ static sqlite3_stmt *statement = nil;
     }
     
     return isSuccess;
+}
+
+-(BOOL) saveData:(NSMutableDictionary*)data{
+    int uid = [[data objectForKey:@"uid"] intValue];
+    NSString *author = [data objectForKey:@"author"];
+    NSString *description = [data objectForKey:@"description"];
+    NSString *image = [data objectForKey:@"image"];
+    NSString *audio = [data objectForKey:@"audio"];
+    NSString *create_date = [data objectForKey:@"create_date"];
+    NSString *update_date = [data objectForKey:@"update_date"];
+    NSString *sync_date = [data objectForKey:@"sync_date"];
+    int liked = [[data objectForKey:@"liked"] intValue];
+    int disliked = [[data objectForKey:@"disliked"] intValue];
+    NSString *remote = [data objectForKey:@"remote"];
+    int enable = [[data objectForKey:@"enable"] intValue];
+    
+    const char *dpath = [databasePath UTF8String];
+    if (sqlite3_open(dpath, &database) == SQLITE_OK){
+        NSString *query = [NSString stringWithFormat:@"INSERT INTO stillsprofile (uid,author,description,image,audio,create_date,update_date,sync_date,liked,disliked,remote,enable) VALUES (\"%d\",\"%@\",\"%@\",\"%@\",\"%@\",\"%@\",\"%@\",\"%@\",\"%d\",\"%d\",\"%@\",\"%d\")",uid,author,description,image,audio,create_date,update_date,sync_date,liked,disliked,remote,enable];
+        const char *query_stmt = [query UTF8String];
+        sqlite3_prepare_v2(database, query_stmt, -1, &statement, NULL);
+        if (sqlite3_step(statement) == SQLITE_DONE){
+            return YES;
+        } else {
+            return NO;
+        }
+        sqlite3_reset(statement);
+    }
+    
+    return NO;
 }
 
 @end
